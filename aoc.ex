a489f40f0f6a7eec30b5cd63fb3963a92de82b81
jordan <sudo.sendmail.jordan@gmail.com>
2025-05-31 17:29:57 -0700

    Add: Day 2 Part A

--- aoc.ex
+++ aoc.ex
@@ -4,8 +4,13 @@ defmodule AOC do
 
     def run do
       IO.puts("Advent of Code 2021\n")
+
         # Solutions.Day1.partA()
-        Solutions.Day1.partB()
+        # Solutions.Day1.partB()
+
+        Solutions.Day2.partA()
+        # Solutions.Day2.partB()
+
     end
 
   end
--- lib/solutions/day_2.ex
+++ lib/solutions/day_2.ex
@@ -1,11 +1,41 @@
   defmodule Solutions.Day2 do
 
     def partA do
-      IO.puts("Day 2 Part A Solution")
-    end
+      IO.puts("| Day 2 | Part A | Solution \n")
+
+      result = "data/2_input.txt"
+        |> File.stream!()
+        |> Stream.map(&String.trim/1)
+        |> Stream.map(&String.split(&1, " "))
+        |> Enum.count(fn [count, letter, password] ->
+          [min, max] = count
+            |> String.split("-")
+            |> Enum.map(&String.to_integer/1)
+          count_in_password = String.graphemes(password)
+            |> Enum.count(&(&1 == String.replace(letter, ":", "")))
+          count_in_password >= min and count_in_password <= max
+        end)
+
+        IO.puts("Result: #{result}")
+      end
 
     def partB do
-      IO.puts("Day 2 Part B Solution")
+      IO.puts("| Day 2 | Part B | Solution \n")
+
+      result = "data/2_sample.txt"
+        |> File.stream!()
+        |> Stream.map(&String.trim/1)
+        |> Stream.map(&String.split(&1, " "))
+        |> Enum.count(fn [count, letter, password] ->
+          [min, max] = count
+            |> String.split("-")
+            |> Enum.map(&String.to_integer/1)
+          count_in_password = String.graphemes(password)
+            |> Enum.count(&(&1 == String.replace(letter, ":", "")))
+          count_in_password >= min and count_in_password <= max
+        end)
+
+        IO.puts("Result: #{result}")
     end
 
   end
